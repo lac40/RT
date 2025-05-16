59b7314a4d632a6752371ee3cd22d6cca3366cca
Kornis,Laszlo L.B. <l.kornis@student.fontys.nl>
2025-05-16 13:00:59 +0200

    Added solution and MVC project

---
+++ RepTrack/RepTrackWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepTrackWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
