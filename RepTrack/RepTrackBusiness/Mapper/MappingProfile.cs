5616d9bd7c0e7e585a3ede3da5896b49b7e6ba09
Kornis,Laszlo L.B. <l.kornis@student.fontys.nl>
2025-05-16 15:31:01 +0200

    Refactor mapping structure and add AutoMapper profiles
    Removed "Mapper" folder reference from RepTrackBusiness.csproj to restructure project organization.
    
    Introduced MappingProfile class in MappingProfile.cs to define mappings between domain models and DTOs using AutoMapper. Added necessary using directives and specified property mappings for WorkoutSession, WorkoutExercise, ExerciseSet, and Exercise.

---
+++ RepTrack/RepTrackBusiness/Mapper/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using RepTrackBusiness.DTOs;
+using RepTrackDomain.Models;
+
+namespace RepTrackBusiness.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Map from domain models to DTOs
+            CreateMap<WorkoutSession, WorkoutSessionDto>()
+                .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags));
+
+            CreateMap<WorkoutExercise, WorkoutExerciseDto>()
+                .ForMember(dest => dest.ExerciseName, opt => opt.MapFrom(src => src.Exercise.Name));
+
+            CreateMap<ExerciseSet, ExerciseSetDto>();
+
+            CreateMap<Exercise, ExerciseDto>()
+                .ForMember(dest => dest.SecondaryMuscleGroups, opt => opt.MapFrom(src => src.SecondaryMuscleGroups));
+
+            // Map from DTOs to domain models (if needed)
+            // These would be more complex and typically involve CreateMap<WorkoutSessionDto, WorkoutSession>() etc.
+        }
+    }
+}
