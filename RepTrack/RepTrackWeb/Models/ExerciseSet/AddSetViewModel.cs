f96011de13582a089fceae668e973859899947ae
Kornis,Laszlo L.B. <l.kornis@student.fontys.nl>
2025-05-18 19:11:49 +0200

    Add view models for exercise sets and workouts
    Updated `AddSetViewModel.cs` to manage exercise sets with properties for workout details, set type, weight, repetitions, RPE, order, and completion status. Added data annotations for validation and display.
    
    Modified `AddExerciseViewModel.cs` to handle exercises within a workout, including properties for workout ID, exercise ID, order, and notes. Included selectable exercises and added necessary data annotations for improved user input handling.

---
+++ RepTrack/RepTrackWeb/Models/ExerciseSet/AddSetViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using RepTrackDomain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace RepTrackWeb.Models.ExerciseSet
+{
+    public class AddSetViewModel
+    {
+        public int WorkoutExerciseId { get; set; }
+
+        public int WorkoutId { get; set; }
+
+        [Required]
+        [Display(Name = "Set Type")]
+        public SetType Type { get; set; }
+
+        [Required]
+        [Display(Name = "Weight")]
+        [Range(0, 999.99)]
+        public decimal Weight { get; set; }
+
+        [Required]
+        [Display(Name = "Repetitions")]
+        [Range(0, 999)]
+        public int Repetitions { get; set; }
+
+        [Required]
+        [Display(Name = "RPE")]
+        [Range(0, 10.0)]
+        public decimal RPE { get; set; }
+
+        [Display(Name = "Order in Exercise")]
+        public int OrderInExercise { get; set; } = 1;
+
+        [Display(Name = "Set Completed")]
+        public bool IsCompleted { get; set; }
+
+        public List<SelectListItem> SetTypes { get; set; } = new List<SelectListItem>();
+    }
+}
---
+++ RepTrack/RepTrackWeb/Models/WorkoutExercise/AddExerciseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace RepTrackWeb.Models.WorkoutExercise
+{
+    public class AddExerciseViewModel
+    {
+        public int WorkoutId { get; set; }
+
+        [Required]
+        [Display(Name = "Exercise")]
+        public int ExerciseId { get; set; }
+
+        [Display(Name = "Order in Workout")]
+        public int OrderInWorkout { get; set; } = 0;
+
+        [Display(Name = "Notes")]
+        [MaxLength(200)]
+        public string Notes { get; set; }
+
+        public List<SelectListItem> Exercises { get; set; } = new List<SelectListItem>();
+    }
+}
